f59ba4ef4502e8da0cc742a332ea604b77d67f8a
Matheus Matos <57081161+mmdes@users.noreply.github.com>
2025-07-13 20:44:33 -0400

    .

--- dashboard/pages/eda.py
+++ dashboard/pages/eda.py
@@ -191,13 +191,118 @@ def show():
     with open('./dashboard/assets/resultados_grid_search_cv_v6.pkl', 'rb') as arquivo:
         objeto = pickle.load(arquivo)
 
-    if arquivo:
+    # Colocando os resultados do treinamento com cross validation e grid search 
+    # estático pois o treinamento é demorado. Assim não será obrigatório treinar
+    # com tuning de modelo e parâmetro para ter acesso a esses resultados
 
-        resultados = objeto
+    resultados = {'XGBoost': {'Melhores parâmetros': {'clf__colsample_bytree': 0.6,
+   'clf__gamma': 0,
+   'clf__learning_rate': 0.1,
+   'clf__max_depth': 10,
+   'clf__min_child_weight': 1,
+   'clf__n_estimators': 400,
+   'clf__reg_alpha': 0.5,
+   'clf__reg_lambda': 1,
+   'clf__subsample': 0.8},
+  'ROC AUC (validação)': 0.9614262293714677,
+  'ROC AUC (teste)': 0.95959412967073,
+  'Classification Report': {'0': {'precision': 0.9755051606338131,
+    'recall': 0.9814259597806215,
+    'f1-score': 0.9784566033609157,
+    'support': 13675.0},
+   '1': {'precision': 0.7462537462537463,
+    'recall': 0.6891143911439115,
+    'f1-score': 0.7165467625899281,
+    'support': 1084.0},
+   'accuracy': 0.9599566366284978,
+   'macro avg': {'precision': 0.8608794534437797,
+    'recall': 0.8352701754622665,
+    'f1-score': 0.8475016829754218,
+    'support': 14759.0},
+   'weighted avg': {'precision': 0.9586673983743109,
+    'recall': 0.9599566366284978,
+    'f1-score': 0.9592201871134903,
+    'support': 14759.0}},
+  'Confusion Matrix': [[13421, 254], [337, 747]]},
+ 'RandomForest': {'Melhores parâmetros': {'clf__bootstrap': False,
+   'clf__max_depth': 20,
+   'clf__max_features': 'sqrt',
+   'clf__min_samples_leaf': 1,
+   'clf__min_samples_split': 5,
+   'clf__n_estimators': 300},
+  'ROC AUC (validação)': 0.9617971360742456,
+  'ROC AUC (teste)': 0.962140929727396,
+  'Classification Report': {'0': {'precision': 0.9807649789962415,
+    'recall': 0.973162705667276,
+    'f1-score': 0.976949053002496,
+    'support': 13675.0},
+   '1': {'precision': 0.6915966386554622,
+    'recall': 0.7592250922509225,
+    'f1-score': 0.723834652594547,
+    'support': 1084.0},
+   'accuracy': 0.9574496917135308,
+   'macro avg': {'precision': 0.8361808088258518,
+    'recall': 0.8661938989590993,
+    'f1-score': 0.8503918527985215,
+    'support': 14759.0},
+   'weighted avg': {'precision': 0.9595265156227472,
+    'recall': 0.9574496917135308,
+    'f1-score': 0.9583586329169742,
+    'support': 14759.0}},
+  'Confusion Matrix': [[13308, 367], [261, 823]]},
+ 'LogisticRegression': {'Melhores parâmetros': {'clf__C': 100.0},
+  'ROC AUC (validação)': 0.8665501989874699,
+  'ROC AUC (teste)': 0.8681612552871416,
+  'Classification Report': {'0': {'precision': 0.9775204359673024,
+    'recall': 0.8394881170018281,
+    'f1-score': 0.9032613399425626,
+    'support': 13675.0},
+   '1': {'precision': 0.27197346600331673,
+    'recall': 0.7564575645756457,
+    'f1-score': 0.4000975847767748,
+    'support': 1084.0},
+   'accuracy': 0.8333897960566434,
+   'macro avg': {'precision': 0.6247469509853096,
+    'recall': 0.797972840788737,
+    'f1-score': 0.6516794623596687,
+    'support': 14759.0},
+   'weighted avg': {'precision': 0.9257003319330888,
+    'recall': 0.8333897960566434,
+    'f1-score': 0.8663056172919961,
+    'support': 14759.0}},
+  'Confusion Matrix': [[11480, 2195], [264, 820]]},
+ 'LightGBM': {'Melhores parâmetros': {'clf__colsample_bytree': 0.6,
+   'clf__learning_rate': 0.1,
+   'clf__min_child_samples': 50,
+   'clf__n_estimators': 200,
+   'clf__num_leaves': 100,
+   'clf__subsample': 0.6},
+  'ROC AUC (validação)': 0.9590157460213717,
+  'ROC AUC (teste)': 0.9577255678406876,
+  'Classification Report': {'0': {'precision': 0.9750835391544385,
+    'recall': 0.9815722120658136,
+    'f1-score': 0.9783171167231515,
+    'support': 13675.0},
+   '1': {'precision': 0.7462235649546828,
+    'recall': 0.683579335793358,
+    'f1-score': 0.7135291285507944,
+    'support': 1084.0},
+   'accuracy': 0.9596856155566095,
+   'macro avg': {'precision': 0.8606535520545606,
+    'recall': 0.8325757739295858,
+    'f1-score': 0.845923122636973,
+    'support': 14759.0},
+   'weighted avg': {'precision': 0.9582745268885305,
+    'recall': 0.9596856155566095,
+    'f1-score': 0.958869310016814,
+    'support': 14759.0}},
+  'Confusion Matrix': [[13423, 252], [343, 741]]}}
+
+
 
         # --- Seleção de modelo ---
-        modelo_escolhido = st.selectbox("Selecione o modelo:", list(resultados.keys()))
-        dados = resultados[modelo_escolhido]
+    modelo_escolhido = st.selectbox("Selecione o modelo:", list(resultados.keys()))
+    dados = resultados[modelo_escolhido]
 
         # --- Exibe os melhores parâmetros ---
         # st.subheader("Melhores Parâmetros")
@@ -205,37 +310,37 @@ def show():
 
         # --- Exibe os scores principais ---
         #st.subheader("Principais Métricas")
-        col1, col2, col3 = st.columns(3)
+    col1, col2, col3 = st.columns(3)
 
-        col1.metric("ROC AUC (Validação)", f"{dados['ROC AUC (validação)']:.4f}")
-        col2.metric("ROC AUC (Teste)", f"{dados['ROC AUC (teste)']:.4f}")
-        col3.metric("Acurácia", f"{dados['Classification Report']['accuracy']:.4f}")
+    col1.metric("ROC AUC (Validação)", f"{dados['ROC AUC (validação)']:.4f}")
+    col2.metric("ROC AUC (Teste)", f"{dados['ROC AUC (teste)']:.4f}")
+    col3.metric("Acurácia", f"{dados['Classification Report']['accuracy']:.4f}")
 
         # --- Gráfico de métricas por classe ---
-        st.subheader("Precision, Recall e F1-score por Classe")
+    st.subheader("Precision, Recall e F1-score por Classe")
 
-        report = dados['Classification Report']
-        classes = ['0', '1']
-        metricas = ['precision', 'recall', 'f1-score']
+    report = dados['Classification Report']
+    classes = ['0', '1']
+    metricas = ['precision', 'recall', 'f1-score']
 
-        dados_plot = pd.DataFrame({
+    dados_plot = pd.DataFrame({
             'Classe': [],
             'Métrica': [],
             'Valor': []
         })
 
-        for classe in classes:
-            for metrica in metricas:
-                dados_plot = pd.concat([
-                    dados_plot,
-                    pd.DataFrame({
-                        'Classe': [classe],
-                        'Métrica': [metrica],
-                        'Valor': [report[classe][metrica]]
-                    })
-                ], ignore_index=True)
+    for classe in classes:
+        for metrica in metricas:
+            dados_plot = pd.concat([
+                dados_plot,
+                pd.DataFrame({
+                    'Classe': [classe],
+                    'Métrica': [metrica],
+                    'Valor': [report[classe][metrica]]
+                })
+            ], ignore_index=True)
 
-        fig_metrics = px.bar(
+    fig_metrics = px.bar(
             dados_plot,
             x='Métrica',
             y='Valor',
@@ -243,19 +348,19 @@ def show():
             barmode='group',
             title='Métricas por Classe'
         )
-        st.plotly_chart(fig_metrics, use_container_width=True)
+    st.plotly_chart(fig_metrics, use_container_width=True)
 
         # --- Matriz de Confusão com Plotly ---
-        st.subheader("Matriz de confusão")
+    st.subheader("Matriz de confusão")
 
         # Extrai a matriz de confusão dos seus dados
-        conf_matrix = dados['Confusion Matrix']
+    conf_matrix = dados['Confusion Matrix']
 
         # Define os rótulos dos eixos
-        axis_labels = ['Classe 0', 'Classe 1']
+    axis_labels = ['Classe 0', 'Classe 1']
 
         # Cria a figura da matriz de confusão com plotly.express
-        fig_conf_plotly = px.imshow(
+    fig_conf_plotly = px.imshow(
             conf_matrix,
             labels=dict(x="Valores Previstos", y="Valores Reais", color="Contagem"),
             x=axis_labels,
@@ -265,13 +370,13 @@ def show():
         )
 
         # Centraliza o título e ajusta o layout
-        fig_conf_plotly.update_layout(
+    fig_conf_plotly.update_layout(
             title_text='<b>Matriz de Confusão</b>',
             title_x=0.5
         )
 
         # Exibe o gráfico no Streamlit
-        st.plotly_chart(fig_conf_plotly, use_container_width=True)
+    st.plotly_chart(fig_conf_plotly, use_container_width=True)
 
     st.markdown("---")
     
